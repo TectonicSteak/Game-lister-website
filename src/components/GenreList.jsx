b168250724d7db4ecc75a090dd50de75c724dacc
TectonicSteak <adithyasp2003@gmail.com>
2023-10-15 00:08:59 +0530

    updated files

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -11,11 +11,20 @@ const GenreList = () => {
     const getGenreList = () => {
         GlobalApi.getGenreList.then((response)=>{
             setGenreList(response.data.results)
+            console.log(response.data.results);
         })
     }
 
     return (
-        <div className='text-[30px] font-bold dark:text-white'>Genre</div>
+        <div>
+            <h1 className='text-[30px] font-bold dark:text-white'>Genre</h1>
+            {genreList?.map((item) => (
+                <div className='flex gap-2 items-center mb-2 cursor-pointer hover:bg-gray-300 hover:dark:bg-gray-600 p-2 rounded-lg group'>
+                    <img src={item.image_background} className='w-[40px] h-[40px] object-cover rounded-lg group-hover:scale-105 transition-all ease-out'/>
+                    <h3 className='dark:text-white text-[18px] group-hover:font-bold'>{item.name}</h3>
+                </div>
+            ))}
+        </div>
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ import GenreList from '../components/GenreList'
 
 function Home() {
   return (
-    <div className="grid grid-cols-4">
+    <div className="grid grid-cols-6 px-5">
       <div className="hidden md:block">
         <GenreList/> 
       </div>
-      <div className="coll-span-4 md:col-span-3">Game List</div>
+      <div className="coll-span-6 md:col-span-5">Game List</div>
     </div>
   )
 }
